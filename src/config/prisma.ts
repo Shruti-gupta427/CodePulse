8a539ac1518d1de6e9bcb76d7f9630b7bcf556fc
shrutiXcode <sg920450@gmail.com>
2026-06-16 23:01:02 +0530

    made changes during testing

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,5 +1,7 @@
+import 'dotenv/config'
 import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
+console.log('DATABASE_URL:', process.env.DATABASE_URL)
 
 const adapter = new PrismaPg({ 
   connectionString: process.env.DATABASE_URL! 
